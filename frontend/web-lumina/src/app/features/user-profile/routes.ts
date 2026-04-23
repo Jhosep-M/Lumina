124fe49777757dcd0a50ad56ad31715aeca5e3df
Jhosep-M <molloemerson10@gmail.com>
2026-04-23 10:46:53 -0400

    perfil-3_Error

--- frontend/web-lumina/src/app/features/user-profile/routes.ts
+++ frontend/web-lumina/src/app/features/user-profile/routes.ts
@@ -1,9 +1,6 @@
-// src/app/features/user-profile/routes.ts
 import { Routes } from '@angular/router';
-// TODO: crear ProfileComponent en features/user-profile/profile/
-// import { ProfileComponent } from './profile/profile.component';
+import { ProfileComponent } from './profile/profile.component';
 
 export const PROFILE_ROUTES: Routes = [
-  // { path: '', component: ProfileComponent }
-  { path: '', redirectTo: '/courses', pathMatch: 'full' } // temporal
+  { path: '', component: ProfileComponent }
 ];
